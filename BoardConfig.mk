f2e84ea61b8704b01a2dc2a756d7de12595258d1
ludevjhon <70524137+ludevjhon@users.noreply.github.com>
2020-09-08 08:15:37 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,17 @@
 DEVICE_PATH := device/motorola/def
 BOARD_VENDOR := motorola
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
 
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a9
 # Security patch level
 VENDOR_SECURITY_PATCH := 2019-10-05
 
